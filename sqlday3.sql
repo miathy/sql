d30d9933c061c7aa50e9761756613d49af4d907c
miathy <72605046+miathy@users.noreply.github.com>
2023-06-02 14:33:59 +1000

    Update sqlday3.sql

--- sqlday3.sql
+++ sqlday3.sql
@@ -11,23 +11,22 @@ from film
 where film.length_min > (select avg(length_min) from film);
 
 -- 3/ Get the room which have the highest and lowest screenings IN 1 SQL query
-select room_id, count
-from (select count(id) as count, room_id
+select room_id, count(id) as count
 from screening
-group by room_id) as t
-having count = (select max(count)
-						from  (select count(id) as count, room_id
-								from screening
-								group by room_id) as k)
-		or count = (select min(count)
-						from  (select count(id) as count, room_id
-								from screening
-								group by room_id) as k)
+group by room_id
+having count= (select count(id) as count from screening
+								group by room_id
+                                order by count desc
+                                limit 1)
+		or count = (select count(id) as count from screening
+								group by room_id
+                                order by count asc
+                                limit 1)
 order by count
 ;
 
 -- 4/ Get number of booking customers of each room of film ‘Tom&Jerry’
-select room_id, count(customer_id)
+select room_id, count(*)
 from screening join booking on screening.id = booking.screening_id
 				join film on film.id = screening.film_id
 where film.name ='Tom&Jerry'
@@ -44,32 +43,29 @@ from ( select seat_id, count(id) as count from reserved_seat
 select film_id, count
 from (select film_id, count(*) as count
 			from screening 
-			where start_time between '2022-01-01 00:00:00' and '2022-12-30 00:00:00'
+			where year(start_time) =2022
 			group by film_id) as t
 having count = (select max(count) from (select film_id, count(*) as count
 							from screening 
-							where start_time between '2022-01-01 00:00:00' and '2022-12-30 00:00:00'
+							where year(start_time) =2022
 							group by film_id) as t);
 ;
-select film_id, count(*) as count
-			from screening 
-			where start_time between '2022-01-01 00:00:00' and '2022-12-30 00:00:00'
-			group by film_id ;
+
 -- 7/ Which day has most screen?
 
-select start_time, count
-from (select start_time, count(id) as count
-			from screening
-			group by date(start_time)) as t
+select date(start_time), count(*) as count
+from screening 
+group by date(start_time)
 having count = (select max(count) from (select start_time, count(id) as count
 			from screening
 			group by date(start_time)) as t);
 ;
+
 -- 8/ Show film on 2022- MAy
-select id, start_time
-from screening
-where extract( year from start_time)  =2022
-and extract(month from start_time) = 05;
+select distinct film.name
+from screening join film on film.id = screening.film_id
+where  year(start_time)  =2022
+and month (start_time) = 5;
 	
 -- 9/ Select film end with character "n"
 select name
@@ -79,7 +75,7 @@ where name like '%n';
 -- 10/ Show customer name but just show first 3 characters AND last 3 characters in UPPERCASE
 select upper(substring(first_name, 1,3)) as firstName,   upper(substring(last_name, 1,3)) as lastName 
 from customer;
--- 11/ WHAT film long than 2 hours?
+-- 11/ WHAT film longer than 2 hours?
 select name, length_min
 from film
 where length_min >120;
